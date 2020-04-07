ad9260e310c7a3b610ed17d6c30f448953c1f1db
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-07 17:59:08 +0100

    setup ps1 order of sequence

--- UserGroup.SQL/Setup.ps1
+++ UserGroup.SQL/Setup.ps1
@@ -1,1 +1,18 @@
-﻿Invoke-SqlCmd -ServerInstance -ANKANHOME\tupai -InputFile C:\Development\Eintech\PersonGroup-Db.sql
+﻿# Clone from Git into a directory
+
+#Build Solution
+
+#Create Database
+Invoke-SqlCmd -ServerInstance -ANKANHOME\tupai -InputFile C:\Development\Eintech\UserGroups\Create.sql
+
+#Apply Migration
+	#Use the back up db provided in the solution to apply migration
+
+#Restore Nuget from packages
+
+#Build Solution
+
+#Run yarn or npm if needed
+
+#Run Soution
+
