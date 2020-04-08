8f9b25e86ec3036c94e196b73d5d9060f3564f46
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-08 01:36:30 +0100

    added basic api controllers

---
+++ UserGroup.Web/Controllers/GroupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserGroup.Web.Models;
+
+
+namespace UserGroup.Web.Controllers
+{
+    [ApiController]
+    [Route("api/groups")]
+    public class GroupController
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "Sypderman", Group = "DC", DateAdded = DateTime.UtcNow },
+                    new {id = 1, Name = "Batman", Group = "Marvel", DateAdded = DateTime.UtcNow}
+                }
+                );
+        }
+    }
+}
---
+++ UserGroup.Web/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserGroup.Web.Models;
+
+namespace UserGroup.Web.Controllers
+{
+    [ApiController]
+    [Route("api/person")]
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "DC"},
+                    new {id = 1, Name = "Marvel"}
+                }
+                );
+        }
+    }
+}
