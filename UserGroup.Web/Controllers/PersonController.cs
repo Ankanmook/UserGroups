657bbe42513a06a5e2898f0b534bbd15553cb156
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-08 12:59:17 +0100

    added nlog

--- UserGroup.Web/Controllers/PersonController.cs
+++ UserGroup.Web/Controllers/PersonController.cs
@@ -15,6 +15,13 @@ namespace UserGroup.Web.Controllers
     [Route("api/person")]
     public class PersonController : Controller
     {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+        }   
+        
         [HttpGet (Name = "Get")]
         public IActionResult Get()
         {
@@ -29,11 +36,22 @@ namespace UserGroup.Web.Controllers
         }
 
         [HttpGet("{id}", Name = "GetPerson")]
-        public IActionResult GetPerson()
+        public IActionResult GetPerson(int id)
         {
-            return Ok(
+            try
+            {
+                return Ok(
                new { id = 1, Name = "Sypderman", Group = "DC", DateAdded = DateTime.UtcNow }
                 );
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception happened getting person with id: {id}", ex);
+                return StatusCode(500, "A problem happened while handling your request");
+
+            }
+
         }
 
         [HttpPost]
@@ -55,7 +73,6 @@ namespace UserGroup.Web.Controllers
         public IActionResult Put(int id, PersonForUpdateDto personCreationDto)
         {
             
-
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -90,26 +107,54 @@ namespace UserGroup.Web.Controllers
                 return NotFound();
             }
 
-            var personToPatch = new PersonForUpdateDto()
+            //from repository
+            var personFromStore = new PersonDto()
             {
 
             };
 
-            //patchDoc.ApplyTo(personToPatch, ModelState);
+            var personToPatch = new PersonForUpdateDto()
+            {
+                Name = personFromStore.Name,
+                DateAdded = personFromStore.DateAdded,
+                GroupId = personFromStore.GroupId
+            };
+
+            patchDoc.ApplyTo(personToPatch, ModelState);
+
 
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            //for validating the person entity which needed patching [accidentally making the fields invalid]
+            if (!TryValidateModel(personToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            //update the person
+
             return NoContent();
 
         }
 
-        [HttpDelete]
-        public IActionResult Delete()
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
         {
-            throw new NotImplementedException();
+            var person = new PersonDto(); //get this from service
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            //delete the person
+
+            return NoContent();
+
         }
     }
 }
