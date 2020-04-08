91d9666dab8fa57b6f208a961c1090c970118bb6
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-08 13:18:48 +0100

    added logging NLog

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UserGroups
+
+Added NLog Logging 
--- UserGroup.Web/Program.cs
+++ UserGroup.Web/Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using NLog.Web;
+using System;
 
 namespace UserGroup.Web
 {
@@ -14,7 +9,24 @@ namespace UserGroup.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder
+                        .ConfigureNLog("nlog.config")
+                        .GetCurrentClassLogger();
+
+            try
+            {
+                logger.Info("Initializing application...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Application stopped bacuase of excetion");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
