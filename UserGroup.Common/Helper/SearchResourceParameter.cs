f2034be9b10b2dda6e534e189d394a849fb9a5fe
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-09 12:15:56 +0100

    cleanup search resource parameter

--- UserGroup.Common/Helper/SearchResourceParameter.cs
+++ UserGroup.Common/Helper/SearchResourceParameter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UserGroup.Common.Enums;
+﻿using UserGroup.Common.Enums;
 using UserGroup.DataModel.Helpers;
 
 namespace UserGroup.Common.Helper
@@ -11,7 +6,6 @@ namespace UserGroup.Common.Helper
     public class SearchResourceParameter : ResourceParameters
     {
         public SearchOption SearchOption { get; set; } = SearchOption.Dapper;//set to default
-
         public string Name { get; set; }
         public string Group { get; set; }
     }
