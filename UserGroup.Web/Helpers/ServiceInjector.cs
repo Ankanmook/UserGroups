1b6066cee9f1504a8184f15d7608397c5052e579
Ankan Mookherjee <tupaimook@gmail.com>
2020-04-12 17:07:08 +0100

    service injector

---
+++ UserGroup.Web/Helpers/ServiceInjector.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using UserGroup.Common.Contracts;
+using UserGroup.DAL;
+using UserGroup.Services;
+
+namespace UserGroup.Web.Helpers
+{
+    public static class ServiceInjector
+    {
+        public static void AddConfiguredService(IServiceCollection services)
+        {
+            ConfigureRepositories(services);
+            ConfigureServiceLayer(services);
+            ConfigureAutomapper(services);
+        }
+
+        private static void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<ISearchRepository, SearchRepository>();
+        }
+
+        private static void ConfigureServiceLayer(IServiceCollection services)
+        {
+            services.AddScoped<IPersonService, PersonService>();
+            services.AddScoped<IGroupService, GroupService>();
+            services.AddScoped<ISearchService, SearchService>();
+        }
+
+        private static void ConfigureAutomapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
--- UserGroup.Web/Startup.cs
+++ UserGroup.Web/Startup.cs
@@ -1,30 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using UseGroup.DataModel.Models;
-using Microsoft.Extensions.Configuration.Json;
-using UserGroup.Common.Contracts;
-using UserGroup.DAL;
-using UserGroup.Services;
-using AutoMapper;
 using UserGroup.Common;
-using Microsoft.Extensions.FileProviders;
-using System.IO;
 using UserGroup.Web.Helpers;
 
 namespace UserGroup.Web
 {
     public class Startup
     {
-
         //public IConfiguration Configuration { get; }
         private readonly IConfiguration _configuration;
 
@@ -32,35 +20,29 @@ namespace UserGroup.Web
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
-        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOptions();
 
-
             services.AddRazorPages();
 
             services.AddControllersWithViews();
-            
+
             services.AddMvc()
             .AddRazorPagesOptions(options =>
              {
                  options.Conventions.AddPageRoute("/Person/List", "");
              });
 
-
             ConfigureDbContext(services);
-            ConfigureRepositories(services);
-            ConfigureServiceLayer(services);
-            ConfigureAutomapper(services);
+            ServiceInjector.AddConfiguredService(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -105,29 +87,12 @@ namespace UserGroup.Web
 
         public void ConfigureDbContext(IServiceCollection services)
         {
-            var connectionString = _configuration.GetConnectionString(Constants.DbConnectionString);               
-            services.AddDbContext<PersonGroupsContext>(o => {
-                o.UseSqlServer(connectionString);  
+            var connectionString = _configuration.GetConnectionString(Constants.DbConnectionString);
+            services.AddDbContext<PersonGroupsContext>(o =>
+            {
+                o.UseSqlServer(connectionString);
             });
         }
 
-        public void ConfigureRepositories(IServiceCollection services)
-        {
-            services.AddScoped<IPersonRepository, PersonRepository>();
-            services.AddScoped<IGroupRepository, GroupRepository>();
-            services.AddScoped<ISearchRepository, SearchRepository>();
-        }
-
-        public void ConfigureServiceLayer(IServiceCollection services)
-        {
-            services.AddScoped<IPersonService, PersonService>();
-            services.AddScoped<IGroupService, GroupService>();
-            services.AddScoped<ISearchService, SearchService>();
-        }
-
-        public void ConfigureAutomapper(IServiceCollection services)
-        {
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        }
     }
 }
